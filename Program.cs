2249a17d4c0fd32bffeff6e9adeb51efe6714f11
molnartm <molnartm@310-18>
2025-12-01 09:59:14 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Zene_lejatszo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
