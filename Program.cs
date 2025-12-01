a38d4a814fe918f30836d7cec6410b755c3e15aa
molnartm <molnartm@310-18>
2025-12-01 10:01:24 +0100

    Fájl Beolvasása

--- Program.cs
+++ Program.cs
@@ -1,10 +1,35 @@
-﻿namespace Zene_lejatszo
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Zene_lejatszo
 {
     internal class Program
     {
-        static void Main(string[] args)
+        class Zene
         {
-            Console.WriteLine("Hello, World!");
+            public string Cim = "";
+            public string Eloado = "";
+            public string Mufaj = "";
+            public double HosszPerc;
+
+            public override string ToString()
+            {
+                return $"{Cim} - {Eloado} | {Mufaj} | {HosszPerc} perc";
+            }
+        }
+        static void Beolvas(List<Zene> zenek)
+        {
+            string fajl = "C:\\ujmappa\\playlist.txt";
+
+            if (!File.Exists(fajl))
+            {
+                Console.WriteLine("A fájl nem található: " + fajl);
+                Console.WriteLine("Üres lista indul.");
+                Console.ReadKey();
+                return;
+            }
+
         }
     }
 }
